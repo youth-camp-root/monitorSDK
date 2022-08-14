c8dc662bcda1e44d8c7dd1cd1d85722c5d7969a4
gongheng <754097983@qq.com>
2022-08-14 23:58:18 +0800

    first commit

---
+++ src/components/ProfileInfo.vue
@@ -0,0 +1,196 @@
+<template>
+    <div class="sidebar-group mobile-open">
+        <div id="contact-information" class="sidebar active">
+            <header>
+                <span>资料信息</span>
+                <el-button type="danger" @click="closePersonInfo">关闭</el-button>
+            </header>
+            <div class="sidebar-body">
+                <div class="pl-4 pr-4">
+                    <div class="text-center">
+                        <figure class="avatar avatar-xl mb-4">
+                            <img src="/static/dist/media/img/women_avatar5.jpg" class="rounded-circle" alt="image">
+                        </figure>
+                        <h5 class="mb-1">Mirabelle Tow</h5>
+                        <small class="text-muted font-italic">Last seen: Today</small>
+
+                        <ul class="nav nav-tabs justify-content-center mt-5" id="myTab" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab"
+                                   aria-controls="home" aria-selected="true">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab"
+                                   aria-controls="profile" aria-selected="false">Media</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="tab-content" id="myTabContent">
+                        <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                            <p class="text-muted">Lorem ipsum is a pseudo-Latin text used in web design, typography,
+                                layout, and printing in place of English to emphasise design elements over content.
+                                It's also called placeholder (or filler) text. It's a convenient tool for
+                                mock-ups.</p>
+                            <div class="mt-4 mb-4">
+                                <h6>Phone</h6>
+                                <p class="text-muted">(555) 555 55 55</p>
+                            </div>
+                            <div class="mt-4 mb-4">
+                                <h6>City</h6>
+                                <p class="text-muted">Germany / Berlin</p>
+                            </div>
+                            <div class="mt-4 mb-4">
+                                <h6>Website</h6>
+                                <p>
+                                    <a href="#">www.franshanscombe.com</a>
+                                </p>
+                            </div>
+                            <div class="mt-4 mb-4">
+                                <h6 class="mb-3">Social media accounts</h6>
+                                <ul class="list-inline social-links">
+                                    <li class="list-inline-item">
+                                        <a href="#" class="btn btn-sm btn-floating btn-facebook"
+                                           data-toggle="tooltip" title="Facebook">
+                                            <i class="fa fa-facebook"></i>
+                                        </a>
+                                    </li>
+                                    <li class="list-inline-item">
+                                        <a href="#" class="btn btn-sm btn-floating btn-twitter"
+                                           data-toggle="tooltip" title="Twitter">
+                                            <i class="fa fa-twitter"></i>
+                                        </a>
+                                    </li>
+                                    <li class="list-inline-item">
+                                        <a href="#" class="btn btn-sm btn-floating btn-dribbble"
+                                           data-toggle="tooltip" title="Dribbble">
+                                            <i class="fa fa-dribbble"></i>
+                                        </a>
+                                    </li>
+                                    <li class="list-inline-item">
+                                        <a href="#" class="btn btn-sm btn-floating btn-whatsapp"
+                                           data-toggle="tooltip" title="Whatsapp">
+                                            <i class="fa fa-whatsapp"></i>
+                                        </a>
+                                    </li>
+                                    <li class="list-inline-item">
+                                        <a href="#" class="btn btn-sm btn-floating btn-linkedin"
+                                           data-toggle="tooltip" title="Linkedin">
+                                            <i class="fa fa-linkedin"></i>
+                                        </a>
+                                    </li>
+                                    <li class="list-inline-item">
+                                        <a href="#" class="btn btn-sm btn-floating btn-google" data-toggle="tooltip"
+                                           title="Google">
+                                            <i class="fa fa-google"></i>
+                                        </a>
+                                    </li>
+                                    <li class="list-inline-item">
+                                        <a href="#" class="btn btn-sm btn-floating btn-behance"
+                                           data-toggle="tooltip" title="Behance">
+                                            <i class="fa fa-behance"></i>
+                                        </a>
+                                    </li>
+                                    <li class="list-inline-item">
+                                        <a href="#" class="btn btn-sm btn-floating btn-instagram"
+                                           data-toggle="tooltip" title="Instagram">
+                                            <i class="fa fa-instagram"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="mt-4 mb-4">
+                                <h6 class="mb-3">Settings</h6>
+                                <div class="form-group">
+                                    <div class="form-item custom-control custom-switch">
+                                        <input type="checkbox" class="custom-control-input" id="customSwitch11">
+                                        <label class="custom-control-label" for="customSwitch11">Block</label>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="form-item custom-control custom-switch">
+                                        <input type="checkbox" class="custom-control-input" checked=""
+                                               id="customSwitch12">
+                                        <label class="custom-control-label" for="customSwitch12">Mute</label>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="form-item custom-control custom-switch">
+                                        <input type="checkbox" class="custom-control-input" id="customSwitch13">
+                                        <label class="custom-control-label" for="customSwitch13">Get
+                                            notification</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                            <h6 class="mb-3 d-flex align-items-center justify-content-between">
+                                <span>Recent Files</span>
+                                <a href="#" class="btn btn-link small">
+                                    <i data-feather="upload" class="mr-2"></i> Upload
+                                </a>
+                            </h6>
+                            <div>
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item pl-0 pr-0 d-flex align-items-center">
+                                        <a href="#">
+                                            <i class="fa fa-file-pdf-o text-danger mr-2"></i> report4221.pdf
+                                        </a>
+                                    </li>
+                                    <li class="list-group-item pl-0 pr-0 d-flex align-items-center">
+                                        <a href="#">
+                                            <i class="fa fa-image text-muted mr-2"></i> avatar_image.png
+                                        </a>
+                                    </li>
+                                    <li class="list-group-item pl-0 pr-0 d-flex align-items-center">
+                                        <a href="#">
+                                            <i class="fa fa-file-excel-o text-success mr-2"></i>
+                                            excel_report_file2020.xlsx
+                                        </a>
+                                    </li>
+                                    <li class="list-group-item pl-0 pr-0 d-flex align-items-center">
+                                        <a href="#">
+                                            <i class="fa fa-file-text-o text-warning mr-2"></i> articles342133.txt
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import {defineComponent, onMounted, reactive, ref} from "vue";
+    import axios from "axios";
+    import Tool from "@/utils/Tool";
+    import Message from "@/utils/Message";
+    import EIcon from '../components/EIcon.vue';
+    import {useRouter} from "vue-router";
+    export default defineComponent({
+        name: "PersonInfo",
+        props: {
+            EIcon
+        },
+        computed: {
+            computeFileUrl() {
+                return function(url: string) {
+                    return process.env.VUE_APP_SERVER + "/photo/view?filename=" + url;
+                }
+            }
+        },
+        setup(props, context) {
+
+            // 关闭个人信息页面
+            function closePersonInfo() {
+                context.emit("closePersonInfo")
+            }
+
+            return {
+                closePersonInfo
+            }
+        }
+    });
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,12 @@
+<template>
+    <h3>ww</h3>
+    <h3>aa</h3>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+
+});
+</script>
